6fef2cdb6fc27503ef833d5980d4ea64b3c1b4bf
Denis Peshekhonov <denis@peshekhonov.ru>
2025-03-04 16:51:56 +0300

    first version

---
+++ validators/worker_validators.py
@@ -0,0 +1,13 @@
+from typing import List
+from models import TaskDetails
+
+def validate_task_worker_compatibility(task_details: TaskDetails, errors: List[str]):
+    # Проверяем, есть ли тип работ задачи в списке типов работ работника
+    if task_details.task.workTypeId not in task_details.worker.workTypeIds:
+        # Формируем сообщение об ошибке
+        error_message = (
+            f"Задача {task_details.task.id} с типом работ {task_details.task.workTypeId} "
+            f"назначена работнику {task_details.worker.name} ({task_details.worker.id}), "
+            f"у которого нет такого типа работ"
+        )
+        errors.append(error_message)  # Добавляем ошибку в глобальный список
